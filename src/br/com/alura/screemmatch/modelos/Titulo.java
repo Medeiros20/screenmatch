e5c4ba017f50704d4c6c61bb845c0064bbafc676
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2025-11-01 17:59:13 -0300

    Persistindo dados em arquivos

--- src/br/com/alura/screemmatch/modelos/Titulo.java
+++ src/br/com/alura/screemmatch/modelos/Titulo.java
@@ -92,8 +92,8 @@ public class Titulo implements Comparable<Titulo>{
 
     @Override
     public String toString() {
-        return "nome = '" + nome + '\'' +
+        return "( nome = '" + nome +
                 ", anoDeLancamento = " + anoDeLancamento + ", " +
-                "Duração em minutos: " + duracaoEmMinutos;
+                "Duração em minutos: " + duracaoEmMinutos + ") ";
     }
 }
